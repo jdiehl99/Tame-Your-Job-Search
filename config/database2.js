171e3cb56b77fe622c72cbe5e0989bed19ccfbd6
Jodi Diehl <29265610+jdiehl99@users.noreply.github.com>
2017-11-04 19:22:20 -0400

    changed db2 file

---
+++ config/database2.js
@@ -0,0 +1,10 @@
+module.exports = {
+    'connection': {
+        'host': 'x3ztd854gaa7on6s.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+        'user': 'zl925hqm133cy7b8',
+        'password': 'hxjdgbxdpnsle6rp'
+    },
+	'database': 'jnx6dopyogefjios',
+    'users_table': 'users'
+};
+   

7c1593ed40bda0c263a648ee4b5ff78906ef33bd
Jodi Diehl <29265610+jdiehl99@users.noreply.github.com>
2017-11-03 22:13:27 -0400

    user can add new jobs and view details

--- public/js/jobs.js
+++ public/js/jobs.js
@@ -9,30 +9,53 @@ var connection = require("./../../config/connection.js");
 function showPage(app, __dirname) {
 
     // Show index page
-    app.get("/", function (req, res) {
+    app
+        .get("/", function (req, res) {
             res.render("index");
-    });
+        });
 
     // Get list of job from db and send to dashboard template
     app.get("/dashboard", function (req, res) {
-        connection.query("SELECT * FROM jobs;", function (err, data) {
-            if (err) { throw err; }
-            console.log("data from jobs.js",data);
-            res.render("dashboard", { jobs: data });
-        });
+        connection
+            .query("SELECT * FROM jobs;", function (err, data) {
+                if (err) {
+                    throw err;
+                }
+                res.render("dashboard", {jobs: data});
+            });
+    });
+
+    // show form to add job to DB
+    app.get("/addjob", function (req, res) {
+        res.render("addjob");
     });
 
     // Add a new job to the list
     app.post("/job/new", function (req, res) {
         // pick a random number from 1-20 to associate with image
         var job = req.body;
-        console.log("job from jobs.js line 23",job);
-        // connection.query('INSERT INTO jobs (jobtitle) VALUES ("'+job.jobtitle+'")', function (err, result) {
-        //     if (err) { throw err; }
-        //     res.redirect("/");
-        // });
+        // console.log("job from jobs.js line 23", job);
+        console.log("job title",job.jobtitle);
+        connection.query('INSERT INTO jobs (jobtitle,company,contact,phone,email,webpage,source,method) VALUES ("'+job.jobtitle+'","'+job.company+'","'+job.conact+'","'+job.phone+'","'+job.email+'","'+job.webpage+'","'+job.source+'","'+job.method+'")', function (err, result) {     
+            if (err) { console.log(err); }     
+        });
+        res.redirect("/dashboard");
     });
 
+        // Get selected job from DB
+        app.get("/job/:id", function (req, res) {
+            var jobid = req.params.id;
+            console.log("jobid",jobid);
+            connection
+                .query("SELECT * FROM jobs where id = "+jobid+";", function (err, data) {
+                    if (err) {
+                        throw err;
+                    }
+                    console.log("job data",data);
+                    res.render("detail", {jobs: data});
+                });
+        });
+    
 
 }
 module.exports = showPage;

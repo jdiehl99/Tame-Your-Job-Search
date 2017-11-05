b9f10fd8d03390697e800b96c6ce12b441143905
Jodi Diehl <29265610+jdiehl99@users.noreply.github.com>
2017-11-04 23:49:35 -0400

    made some updates to the form CSS

--- public/js/jobs.js
+++ public/js/jobs.js
@@ -110,10 +110,11 @@ module.exports = function (app, passport) {
     });
 
     // show the signup form
-    app.get('/signup', function (req, res) {
-        // render the page and pass in any flash data if it exists
-        res.render('signup');
-    });
+    // app.get('/signup', passport.authenticate('local-signup', {
+    //     successRedirect: '/dashboard', // redirect to the secure profile section
+    //     failureRedirect: '/signup', // redirect back to the signup page if there is an error
+    //     failureFlash: true // allow flash messages
+    // }));
 
     // process the signup form
     app.post('/signup', passport.authenticate('local-signup', {
